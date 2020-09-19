5c0bfda2667150f4df9c13a9f564d73a276bbd3b
Erick Martinez <martinezp.erick@pucp.pe>
2020-09-19 05:46:30 -0500

    Updates

---
+++ precache-manifest.5647fc74490578872f5b6c28e50fa114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba8b779e6d049120e1bfa7a9fa555a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "791e6b4575860ecbdbf3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e98bc20c627c2a744668",
+    "url": "/static/js/2.1ab303eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ab303eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791e6b4575860ecbdbf3",
+    "url": "/static/js/main.6688a2a4.chunk.js"
+  },
+  {
+    "revision": "e93ebe44ad9308cc3b9e",
+    "url": "/static/js/runtime-main.0297c94c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
